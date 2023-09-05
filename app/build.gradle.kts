6a0adb8f8475b48e9d10b3bfd8387c4fa57e387a
coderLady <creativegirl@outlook.fr>
2023-09-05 19:04:23 +0200

    theme light problem solved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("androidx.fragment:fragment:1.6.1")
+    implementation("androidx.navigation:navigation-fragment:2.7.1")
+    implementation("androidx.navigation:navigation-ui:2.7.1")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:2.7.1")
 }
