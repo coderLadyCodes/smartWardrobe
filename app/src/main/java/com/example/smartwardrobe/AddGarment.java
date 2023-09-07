f93dc35005edea64493d8a0b64ed70a071b3c3c6
coderLady <creativegirl@outlook.fr>
2023-09-07 20:33:09 +0200

    add item screen done

--- app/src/main/java/com/example/smartwardrobe/AddGarment.java
+++ app/src/main/java/com/example/smartwardrobe/AddGarment.java
@@ -2,7 +2,10 @@ package com.example.smartwardrobe;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,15 +22,20 @@ public class AddGarment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentAddGarmentBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.addgarment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_addGarment_to_chooseOutfit);
+            }
+        });
     }
 }
