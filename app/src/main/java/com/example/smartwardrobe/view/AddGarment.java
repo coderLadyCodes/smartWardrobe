07fa8f045ab6be4e42820727e0ef864fd4c334b5
coderLady <creativegirl@outlook.fr>
2023-09-16 22:15:34 +0200

    set up ROOM database

--- app/src/main/java/com/example/smartwardrobe/view/AddGarment.java
+++ app/src/main/java/com/example/smartwardrobe/view/AddGarment.java
@@ -10,7 +10,13 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.navigation.Navigation;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
@@ -24,11 +30,18 @@ import android.view.ViewGroup;
 import com.example.smartwardrobe.R;
 import com.example.smartwardrobe.database.GarmentDatabase;
 import com.example.smartwardrobe.databinding.FragmentAddGarmentBinding;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
 
 
 public class AddGarment extends Fragment {
+    private ImageCapture imageCapture;
+    private Camera camera;
     GarmentDatabase garmentDatabase;
     FragmentAddGarmentBinding binding;
+
     public AddGarment() {
 
     }
@@ -56,26 +69,43 @@ public class AddGarment extends Fragment {
             }
         });
 
-        // TAKE IMAGE FROM CAMERA
+        // TAKE IMAGE FROM camerax
+//
+//        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+//                new ActivityResultContracts.StartActivityForResult(),
+//                result -> {
+//                    if (result.getResultCode() == RESULT_OK && result.getData() != null){
+//                        Bundle bundle = result.getData().getExtras();
+//                        Bitmap bitmap = (Bitmap) bundle.get("data");
+//                        binding.imageuploaded.setImageBitmap(bitmap);
+//                    }
+//                }
+//        );
+//
+//        binding.imagebutton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                activityResultLauncher.launch(intent);
+//            }
+//        });
+    }
 
-        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> {
-                    if (result.getResultCode() == RESULT_OK && result.getData() != null){
-                        Bundle bundle = result.getData().getExtras();
-                        Bitmap bitmap = (Bitmap) bundle.get("data");
-                        binding.imageuploaded.setImageBitmap(bitmap);
-                    }
-                }
-        );
+    private void initializeCamera() {
 
-        binding.imagebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                activityResultLauncher.launch(intent);
-            }
-        });
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build();
 
+                Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider(binding.preView.getSurfaceProvider());
+
+                imageCapture = new ImageCapture.Builder()
+                        .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                        .setTargetRotation(binding.preView.getDisplay().getRotation())
+                        .build();
+
+        camera = Camera.open(cameraSelector);
+        camera.setPreview(binding.preView.getSurfaceProvider());
     }
 }
