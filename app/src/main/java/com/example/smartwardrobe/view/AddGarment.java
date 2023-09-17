ae5f9c88b1d958cae6f3ac24111f59dc2da38c88
coderLady <creativegirl@outlook.fr>
2023-09-17 21:15:14 +0200

    set up ROOM database

--- app/src/main/java/com/example/smartwardrobe/view/AddGarment.java
+++ app/src/main/java/com/example/smartwardrobe/view/AddGarment.java
@@ -2,7 +2,9 @@ package com.example.smartwardrobe.view;
 
 import static android.app.Activity.RESULT_OK;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 
@@ -13,8 +15,11 @@ import androidx.annotation.Nullable;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.navigation.Navigation;
@@ -22,23 +27,31 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 
 import com.example.smartwardrobe.R;
 import com.example.smartwardrobe.database.GarmentDatabase;
 import com.example.smartwardrobe.databinding.FragmentAddGarmentBinding;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 
 public class AddGarment extends Fragment {
-    private ImageCapture imageCapture;
-    private Camera camera;
     GarmentDatabase garmentDatabase;
     FragmentAddGarmentBinding binding;
 
@@ -89,23 +102,5 @@ public class AddGarment extends Fragment {
 //                activityResultLauncher.launch(intent);
 //            }
 //        });
-    }
-
-    private void initializeCamera() {
-
-                CameraSelector cameraSelector = new CameraSelector.Builder()
-                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                        .build();
-
-                Preview preview = new Preview.Builder().build();
-                preview.setSurfaceProvider(binding.preView.getSurfaceProvider());
-
-                imageCapture = new ImageCapture.Builder()
-                        .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                        .setTargetRotation(binding.preView.getDisplay().getRotation())
-                        .build();
-
-        camera = Camera.open(cameraSelector);
-        camera.setPreview(binding.preView.getSurfaceProvider());
     }
 }
