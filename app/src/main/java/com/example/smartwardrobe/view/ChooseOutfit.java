7667e90755ad1ef90ee536f5af18c04d446e49c6
coderLady <creativegirl@outlook.fr>
2023-10-13 06:51:10 +0200

    choose outfit

--- app/src/main/java/com/example/smartwardrobe/view/ChooseOutfit.java
+++ app/src/main/java/com/example/smartwardrobe/view/ChooseOutfit.java
@@ -1,10 +1,12 @@
 package com.example.smartwardrobe.view;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +19,7 @@ import com.example.smartwardrobe.databinding.FragmentChooseOutfitBinding;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ChooseOutfit extends Fragment {
     private String selectedWarmth;
@@ -74,16 +77,59 @@ public class ChooseOutfit extends Fragment {
     }
 
     private Outfit generateOutfit(String warmth, boolean comfort, boolean fancy, boolean loose, String color) {
-        List<Garment> garmentList = sharedViewModel.getGarmentList();
+        List<Garment> garmentList = getWardrobeData(); // Implement this method to get your garment list.
+        Outfit outfit = new Outfit();
+
         // Filter your garment list based on user choices
         List<Garment> filteredGarments = filterGarments(garmentList, warmth, comfort, fancy, loose, color);
+
+        if (filteredGarments.size() > 0) {
+            outfit = selectRandomOutfit(filteredGarments);
+        }
+
+        return outfit;
+    }
+
+    private Outfit selectRandomOutfit(List<Garment> filteredGarments) {
+        Random random = new Random();
         Outfit outfit = new Outfit();
-        assignGarmentsToOutfit(outfit, filteredGarments);
+
+        // Randomly select a top from the filtered garments
+        int topIndex = random.nextInt(filteredGarments.size());
+        outfit.setTop(filteredGarments.get(topIndex));
+
+        filteredGarments.remove(topIndex);
+
+        if (!filteredGarments.isEmpty()) {
+            // Randomly select a bottom from the remaining filtered garments
+            int bottomIndex = random.nextInt(filteredGarments.size());
+            outfit.setBottom(filteredGarments.get(bottomIndex));
+            filteredGarments.remove(bottomIndex);
+        }
+
+        if (!filteredGarments.isEmpty()) {
+            // Randomly select shoes from the remaining filtered garments
+            int shoesIndex = random.nextInt(filteredGarments.size());
+            outfit.setShoes(filteredGarments.get(shoesIndex));
+            filteredGarments.remove(shoesIndex);
+        }
+
+        if (!filteredGarments.isEmpty()) {
+            // Randomly select a coat from the remaining filtered garments
+            int coatIndex = random.nextInt(filteredGarments.size());
+            outfit.setCoat(filteredGarments.get(coatIndex));
+        }
+
         return outfit;
     }
 
     // Implement your filtering logic based on user choices.
     private List<Garment> filterGarments(List<Garment> garmentList, String warmth, boolean comfort, boolean fancy, boolean loose, String color) {
+        // Implement your filtering logic here based on user choices (warmth, comfort, fancy, loose, color).
+        // This can involve iterating through the garmentList and applying filters.
+        // You should return a filtered list of garments that match the user's criteria.
+
+        // Replace this with your actual filtering logic.
         List<Garment> filteredGarments = new ArrayList<>();
         for (Garment garment : garmentList) {
             if (garmentMatchesCriteria(garment, warmth, comfort, fancy, loose, color)) {
@@ -94,26 +140,22 @@ public class ChooseOutfit extends Fragment {
         return filteredGarments;
     }
 
+    // Implement a method to check if a garment matches user criteria.
     private boolean garmentMatchesCriteria(Garment garment, String warmth, boolean comfort, boolean fancy, boolean loose, String color) {
-        boolean warmthMatches = garment.getWarmth().equals(warmth);
-        boolean comfortMatches = garment.isComfort() == comfort;
-        boolean fancyMatches = garment.isFancy() == fancy;
-        boolean colorMatches = garment.getColor().equals(color);
+        // Implement your matching logic here, e.g., checking the warmth level, comfort, color, etc.
+        // You should customize this method based on your garment model.
 
-        return warmthMatches && comfortMatches && fancyMatches && colorMatches;
-
+        // Replace this with your actual matching logic.
+        return garment.getWarmth().equals(warmth)
+                && garment.isComfort() == comfort
+                && garment.isFancy() == fancy
+                && garment.isLoose() == loose
+                && garment.getColor().equals(color);
     }
-    private void assignGarmentsToOutfit(Outfit outfit, List<Garment> garments) {
-        for (Garment garment : garments) {
-                if (garment.getCategorization() == garment.getCategorization().TOP) {
-                outfit.setTop(garment);
-            } else if (garment.getCategorization() == garment.getCategorization().BOTTOM) {
-                outfit.setBottom(garment);
-            } else if (garment.getCategorization() == garment.getCategorization().SHOES) {
-                outfit.setShoes(garment);
-            } else if (garment.getCategorization() == garment.getCategorization().COAT) {
-                outfit.setCoat(garment);
-            }
-        }
+
+    // Implement a method to retrieve your garment list from the ViewModel or data source.
+    private List<Garment> getWardrobeData() {
+        SharedViewModel sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        return sharedViewModel.getGarmentList();
     }
 }
--- app/src/main/java/com/example/smartwardrobe/view/ShowOutfit.java
+++ app/src/main/java/com/example/smartwardrobe/view/ShowOutfit.java
@@ -1,35 +1,107 @@
 package com.example.smartwardrobe.view;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.example.smartwardrobe.R;
+import com.example.smartwardrobe.database.Categorization;
+import com.example.smartwardrobe.database.Garment;
 import com.example.smartwardrobe.databinding.FragmentShowOutfitBinding;
 
-
 public class ShowOutfit extends Fragment {
-
     FragmentShowOutfitBinding binding;
-
+    Outfit chosenOutfit;
 
     public ShowOutfit() {
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            chosenOutfit = getArguments().getParcelable("selectedOutfit");
+        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentShowOutfitBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (chosenOutfit != null) {
+            binding.top.setImageResource(getImageResource(chosenOutfit.getTop()));
+            binding.bottom.setImageResource(getImageResource(chosenOutfit.getBottom()));
+            binding.shoes.setImageResource(getImageResource(chosenOutfit.getShoes()));
+            binding.coat.setImageResource(getImageResource(chosenOutfit.getCoat()));
+        }
+    }
+
+    private int getImageResource(Garment garment) {
+        if (garment != null) {
+            switch (garment.getCategorization()) {
+                case TOP:
+                    return getTopImageResource(garment);
+                case BOTTOM:
+                    return getBottomImageResource(garment);
+                case SHOES:
+                    return getShoesImageResource(garment);
+                case COAT:
+                    return getCoatImageResource(garment);
+                default:
+                    return getTopImageResource(garment);
+            }
+        } else {
+            return getTopImageResource(null);
+        }
+    }
+
+    private int getTopImageResource(Garment garment) {
+        if (garment != null) {
+            // Implement your logic here to map the top garment to a specific image resource.
+            // For example, you can check the garment's name or category.
+            if (garment.getCategorization() == Categorization.TOP) {
+                return getTopImageResource(garment); // Replace with your image resource.
+            } else if (garment.getCategorization() == Categorization.BOTTOM) {
+                return getTopImageResource(garment); // Replace with your image resource.
+            } else if (garment.getCategorization() == Categorization.SHOES) {
+                return getTopImageResource(garment); // Default image resource.
+            }
+        } else if (garment.getCategorization() == Categorization.COAT){
+
+            return getTopImageResource(garment);
+        } return getTopImageResource(garment);
+    }
+
+
+
+    private int getBottomImageResource(Garment garment) {
+        // Implement your logic to map the bottom garment to a specific image resource.
+        // Return the corresponding image resource ID.
+        return getTopImageResource(garment); // Replace with your image resources.
+    }
+
+    private int getShoesImageResource(Garment garment) {
+        // Implement your logic to map the shoes to a specific image resource.
+        // Return the corresponding image resource ID.
+        return getTopImageResource(garment);// Replace with your image resources.
+    }
+
+    private int getCoatImageResource(Garment garment) {
+        // Implement your logic to map the coat to a specific image resource.
+        // Return the corresponding image resource ID.
+        return getTopImageResource(garment); // Replace with your image resources.
+    }
+
 }
